640d1092bd7be96ecd497b020317f39bd7ede751
acoder1983 <acoder1983@163.com>
2016-03-30 16:17:39 +0800

    Update MainFm.cs

--- ExtractText/MainFm.cs
+++ ExtractText/MainFm.cs
@@ -1,104 +1,39 @@
-using System;
-using System.IO;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 
-using System.Text;
-using System.Windows.Forms;
 
 namespace Stamp4Fun.ExtractText
 {
     public partial class MainFm : Form, Comm.INofifyProgress
     {
-        public MainFm()
-        {
-            InitializeComponent();
-            this.Load += new EventHandler(MainFm_Load);
-        }
-
-        void MainFm_Load(object sender, EventArgs e)
-        {
-            if (this.tbScottFolder.Text.Length > 0)
-            {
-                btnExtract_Click(null, null);
-                Close();
-            }
-        }
-
-        struct PointPair
-        {
-            public Point TopLeft;
-            public Point BottomRight;
-
-            public PointPair(Point a, Point b)
-            {
-                this.TopLeft = a;
-                this.BottomRight = b;
-            }
-        }
-
-        public event ExtractFinishedHandler ExtractFinished;
-        public delegate void ExtractFinishedHandler(ExtractFinishedArgs e);
-        public class ExtractFinishedArgs
-        {
-            public string ScottFolderPath;
-            public Comm.INofifyProgress Sender;
-            public ExtractFinishedArgs(Comm.INofifyProgress sender, string path)
-            {
-                Sender = sender;
-                ScottFolderPath = path;
-            }
-        }
-
         
 
         private void btnExtract_Click(object sender, EventArgs e)
         {
-            if (!Directory.Exists(this.tbScottFolder.Text))
-            {
-                MessageBox.Show("请输入合法路径");
-                return;
-            }
-            this.tbLog.Text = "";
-            this.Hide();
-            string folderpath = this.tbScottFolder.Text;
             
-            this.tbLog.Text = string.Format("selected path: {0}\r\n", folderpath);
-            string fooFile = string.Format("{0}/xxxyyyzzz.pdf", folderpath);
-            if (File.Exists(fooFile))
-            {
-                File.Delete(fooFile);
-            }
-            // read all page names
-            string[] pageFiles = Directory.GetFiles(folderpath, "*.pdf", SearchOption.TopDirectoryOnly);
-            if (pageFiles.Length == 0)
-            {
-                this.tbLog.Text += "there are no pdf files";
-                return;
-            }
-            Array.Sort<string>(pageFiles);
-            
-            // prcess every page
-            PointPair[] pps = new PointPair[]{
-                new PointPair(new Point(660, 167), new Point(810, 1003)),
-                new PointPair(new Point(817, 167), new Point(968, 1003)),
-                new PointPair(new Point(974, 167), new Point(1125, 1003)),
-                new PointPair(new Point(1131, 167), new Point(1282, 1003))
-            };
-
             foreach (string pageFile in pageFiles)
             {
+                // get page num
+                string pageNumStr=pageFile.Substring(pageFile.LastIndexOf(") "));
+                pageNumStr= pageNumStr.Substring(2, pageNumStr.IndexOf(".pdf")-2);
+                // get vol num
+                string volNumStr=pageFile.Substring(pageFile.IndexOf("Volume.")+7,1);
+                // make page folder dir
+                string pageFolderName=string.Format("v{0}-{1}",volNumStr,pageNumStr);
+                string pageFolderPath=folderpath+"/"+pageFolderName;
+                Directory.CreateDirectory(pageFolderPath);
+                // move pdf into dir
+                string newPagePath=pageFolderPath+"/"+pageFolderName+".pdf";
+                File.Copy(pageFile, newPagePath, true);
+
                 int i = 0;
                 foreach (PointPair pp in pps)
                 {
                     // copy a new page
-                    File.Copy(pageFile, fooFile, true);
+                    string fooFile = string.Format("{0}/xxxyyyzzz.pdf", pageFolderPath);
+                    File.Copy(newPagePath, fooFile, true);
 
                     // open it with adobe
                     System.Diagnostics.Process.Start(fooFile);
-                    new KmSim.Delay(5000).Do();
+                    new KmSim.Delay(3000).Do();
 
                     // fit the page size
                     new KmSim.MouseMoveTo(620, 100).Do();
@@ -133,102 +68,55 @@ namespace Stamp4Fun.ExtractText
                     new KmSim.Delay(1000).Do();
                     
                     // save it to txt
-                    new KmSim.KeyPress("%f").Do();
-                    new KmSim.Delay(1000).Do();
-                    new KmSim.KeyPress("t").Do();
-                    new KmSim.Delay(1000).Do();
-                    new KmSim.KeyPress("{DOWN}").Do();
-                    new KmSim.Delay(500).Do();
-                    new KmSim.KeyPress("{DOWN}").Do();
-                    new KmSim.Delay(500).Do();
-                    new KmSim.KeyPress("{DOWN}").Do();
-                    new KmSim.Delay(500).Do();
-                    new KmSim.KeyPress("{DOWN}").Do();
-                    new KmSim.Delay(500).Do();
-                    new KmSim.KeyPress("{DOWN}").Do();
-                    new KmSim.Delay(500).Do();
-                    new KmSim.KeyPress("{RIGHT}").Do();
-                    new KmSim.Delay(500).Do();
-                    new KmSim.KeyPress("{DOWN}").Do();
-                    new KmSim.Delay(500).Do();
-                    new KmSim.KeyPress("{ENTER}").Do();
-                    new KmSim.Delay(1000).Do();
-                    new KmSim.KeyPress(string.Format(@"{2}\{0}.{1}.txt", pageFile.Substring(
-                        pageFile.LastIndexOf(") ")+2), ++i, folderpath)).Do();
+                    string[] keys=["%f","t","{DOWN}","{DOWN}","{DOWN}","{DOWN}","{DOWN}","{RIGHT}","{DOWN}","{ENTER}"];
+                    foreach(string k in keys){
+                        new KmSim.KeyPress(k).Do();
+                        new KmSim.Delay(500).Do();
+                    }
+                    ++i;
+                    new KmSim.KeyPress(string.Format(@"{0}\{1}.txt", pageFolderPath, i)).Do();
                     new KmSim.Delay(1000).Do();
                     new KmSim.KeyPress("{ENTER}").Do();
-                    new KmSim.Delay(5000).Do();
+                    new KmSim.Delay(3000).Do();
+
+                    // save it to jpg
+                    string[] keys=["%f","t","{DOWN}","{DOWN}","{DOWN}","{DOWN}","{DOWN}","{RIGHT}","{DOWN}","{ENTER}"];
+                    foreach(string k in keys){
+                        new KmSim.KeyPress(k).Do();
+                        new KmSim.Delay(500).Do();
+                    }
+                    new KmSim.KeyPress(string.Format(@"{0}\{1}.jpg", pageFolderPath, i)).Do();
+                    new KmSim.Delay(1000).Do();
+                    new KmSim.KeyPress("{ENTER}").Do();
+                    new KmSim.Delay(3000).Do();
 
                     // close the page
                     new KmSim.KeyPress("%{F4}").Do();
-                    new KmSim.Delay(1000).Do();
+                    new KmSim.Delay(2000).Do();
                 }
 
                 // open it with adobe
                 System.Diagnostics.Process.Start(pageFile);
-                new KmSim.Delay(5000).Do();
+                new KmSim.Delay(3000).Do();
 
                 // save it to tiff
-                new KmSim.KeyPress("%f").Do();
-                new KmSim.Delay(1000).Do();
-                new KmSim.KeyPress("t").Do();
-                new KmSim.Delay(1000).Do();
-                new KmSim.KeyPress("{DOWN}").Do();
-                new KmSim.Delay(500).Do();
-                new KmSim.KeyPress("{DOWN}").Do();
-                new KmSim.Delay(500).Do();
-                new KmSim.KeyPress("{DOWN}").Do();
-                new KmSim.Delay(500).Do();
-                new KmSim.KeyPress("{DOWN}").Do();
-                new KmSim.Delay(500).Do();
-                new KmSim.KeyPress("{RIGHT}").Do();
-                new KmSim.Delay(500).Do();
-                new KmSim.KeyPress("{DOWN}").Do();
-                new KmSim.Delay(500).Do();
-                new KmSim.KeyPress("{DOWN}").Do();
-                new KmSim.Delay(500).Do();
-                new KmSim.KeyPress("{DOWN}").Do();
-                new KmSim.Delay(500).Do();
-                new KmSim.KeyPress("{ENTER}").Do();
-                new KmSim.Delay(1000).Do();
-                new KmSim.KeyPress(string.Format(@"{1}\{0}.tiff", pageFile.Substring(
-                    pageFile.LastIndexOf(") ") + 2), folderpath)).Do();
+                string[] keys=["%f","t","{DOWN}","{DOWN}","{DOWN}","{DOWN}","{RIGHT}","{DOWN}","{DOWN}","{DOWN}","{ENTER}"];
+                foreach(string k in keys){
+                    new KmSim.KeyPress(k).Do();
+                    new KmSim.Delay(500).Do();
+                }
+                new KmSim.KeyPress(string.Format(@"{0}\{1}.jpg", pageFolderPath, pageFolderName)).Do();
                 new KmSim.Delay(1000).Do();
                 new KmSim.KeyPress("{ENTER}").Do();
-                new KmSim.Delay(8000).Do();
+                new KmSim.Delay(5000).Do();
                 // close the page
                 new KmSim.KeyPress("%{F4}").Do();
-                new KmSim.Delay(3000).Do();
+                new KmSim.Delay(2000).Do();
             }
-            File.Delete(fooFile);
-
-            this.Show();
-
-            if (this.ExtractFinished != null)
-            {
-                this.ExtractFinished(new ExtractFinishedArgs(this, folderpath));
-            }
-
-            this.tbLog.Text += "Finshed!";
+           
 
         }
 
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void restoreToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            this.Show();
-        }
-
-        public void Notify(string msg)
-        {
-            this.tbLog.Text += msg + "\r\n";
-            this.tbLog.Refresh();
-            this.tbLog.SelectionLength = this.tbLog.Text.Length;
-            this.tbLog.ScrollToCaret(); 
-        }
+      
     }
 }
