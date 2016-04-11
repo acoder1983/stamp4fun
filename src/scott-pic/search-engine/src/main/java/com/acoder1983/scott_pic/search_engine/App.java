5bfc384beacd7106a5597ebad94849a01b035955
acoder1983 <acoder1983@163.com>
2016-04-11 17:23:27 +0800

    Update App.java

--- src/scott-pic/search-engine/src/main/java/com/acoder1983/scott_pic/search_engine/App.java
+++ src/scott-pic/search-engine/src/main/java/com/acoder1983/scott_pic/search_engine/App.java
@@ -6,7 +6,11 @@ package com.acoder1983.scott_pic.search_engine;
  */
 public class App {
 	public static void main(String[] args) {
-
+        if (args.Length() != 1) {
+            System.out.println("input: scott_nation_path.xml")
+        }
+        String scottNationFile=args[0];
+        search_engine.Builder.build(scottNationFile);
 	}
 
 }
