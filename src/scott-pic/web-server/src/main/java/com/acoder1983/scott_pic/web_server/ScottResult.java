771ffaffe777329affc13dcd98a29acdc633c605
acoder1983 <acoder1983@163.com>
2016-04-12 16:43:29 +0800

    Update ScottResult.java

--- src/scott-pic/web-server/src/main/java/com/acoder1983/scott_pic/web_server/ScottResult.java
+++ src/scott-pic/web-server/src/main/java/com/acoder1983/scott_pic/web_server/ScottResult.java
@@ -1,10 +1,27 @@
 package com.acoder1983.scott_pic.web_server;
 
 public class ScottResult {
-
-	// private final long id;
-	// private final String[] pages;
+        private String errMsg;
+	private ArrayList<String> pages;
 
 	public ScottResult() {
+		errMsg = "";
+		pages = new ArrayList<String>();
+	}
+
+	public String getErrMsg() {
+		return errMsg;
+	}
+
+	public void setErrMsg(String msg) {
+		errMsg = msg;
+	}
+
+	public ArrayList<String> getPages() {
+		return pages;
+	}
+
+	public void setPages(ArrayList<String> pages) {
+		this.pages = pages;
 	}
 }
