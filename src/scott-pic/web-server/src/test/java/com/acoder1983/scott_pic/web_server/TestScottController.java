14d8b2cac0bad2f4c3419d24a16ff85c952804e6
acoder1983 <acoder1983@163.com>
2016-04-12 16:44:31 +0800

    Added files via upload

---
+++ src/scott-pic/web-server/src/test/java/com/acoder1983/scott_pic/web_server/TestScottController.java
@@ -0,0 +1,83 @@
+package test.java.com.acoder1983.scott_pic.web_server;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+import main.java.com.acoder1983.scott_pic.web_server.ScottController;
+
+public class TestScottController extends TestCase {
+	@SuppressWarnings("static-access")
+	public void testSplitSearchStr() {
+		ScottController controller = new ScottController();
+		String searchStr = "china 1992";
+		String[] searchKeys = controller.splitSearchStr(searchStr);
+		String[] expect = new String[] { "china", "1992" };
+		this.assertEquals(expect.length, searchKeys.length);
+		for (int i = 0; i < expect.length; i++) {
+			this.assertEquals(expect[i], searchKeys[i]);
+		}
+
+		searchStr = "  look   2016 ";
+		searchKeys = controller.splitSearchStr(searchStr);
+		expect = new String[] { "look", "2016" };
+		this.assertEquals(expect.length, searchKeys.length);
+		for (int i = 0; i < expect.length; i++) {
+			this.assertEquals(expect[i], searchKeys[i]);
+		}
+	}
+
+	@SuppressWarnings("static-access")
+	public void testParsePageFile() {
+		ScottController controller = new ScottController();
+		String pageFile = "catalog/china/002/1.f.txt";
+		this.assertEquals("catalog/china/", controller.parsePagePre(pageFile));
+		this.assertEquals(2, controller.parsePageNum(pageFile));
+		this.assertEquals(1, controller.parseSubPageNum(pageFile));
+	}
+
+	@SuppressWarnings("static-access")
+	public void testPagesBetween() {
+		ScottController controller = new ScottController();
+		String page1 = "catalog/china/002/2.f.txt";
+		String page2 = "catalog/china/003/4.f.txt";
+		ArrayList<String> expect = new ArrayList<String>();
+		expect.add("catalog/china/002/3.f.txt");
+		expect.add("catalog/china/002/4.f.txt");
+		expect.add("catalog/china/003/1.f.txt");
+		expect.add("catalog/china/003/2.f.txt");
+		expect.add("catalog/china/003/3.f.txt");
+
+		this.assertEquals(expect, controller.pagesBetween(page1, page2, "/"));
+	}
+
+	@SuppressWarnings("static-access")
+	public void testParsePages() {
+		ArrayList<String> nationPages = new ArrayList<String>();
+		nationPages.add("catalog/china/002/1.f.txt");
+		nationPages.add("catalog/china/002/2.f.txt");
+		nationPages.add("catalog/china/002/3.f.txt");
+		nationPages.add("catalog/china/002/4.f.txt");
+
+		nationPages.add("catalog/china/003/1.f.txt");
+		nationPages.add("catalog/china/003/2.f.txt");
+		nationPages.add("catalog/china/003/3.f.txt");
+		nationPages.add("catalog/china/003/4.f.txt");
+
+		ArrayList<String> yearPages = new ArrayList<String>();
+		yearPages.add("catalog/china/002/1.f.txt");
+		yearPages.add("catalog/china/002/4.f.txt");
+		yearPages.add("catalog/china/003/2.f.txt");
+		yearPages.add("catalog/china/003/3.f.txt");
+
+		ArrayList<String> expect = new ArrayList<String>();
+		expect.add("catalog/china/002/1.f.txt");
+		expect.add("catalog/china/002/4.f.txt");
+		expect.add("catalog/china/003/1.f.txt");
+		expect.add("catalog/china/003/2.f.txt");
+		expect.add("catalog/china/003/3.f.txt");
+
+		ScottController controller = new ScottController();
+		ArrayList<String> actual = controller.parsePages(nationPages, yearPages, "/");
+		this.assertEquals(expect, actual);
+	}
+}
